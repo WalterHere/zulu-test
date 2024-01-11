edc090a771fc982490d27d12adefc6e068b1f367
WalterHere <walterbrk0085@gmail.com>
2024-01-11 14:52:21 -0500

    added: problem 2

---
+++ src/2.js
@@ -0,0 +1,13 @@
+const powerSet = (arr) => {
+  if (arr.length === 0) {
+    // The power set of an empty array is the array that contains only the empty array
+    return [[]];
+  } else {
+    const [firstNum, ...restNums] = arr;
+    subsets = powerSet(restNums);
+    subsetWithFirstNum = subsets.map((subset) => [firstNum, ...subset]);
+    return [...subsets, ...subsetWithFirstNum];
+  }
+};
+
+console.log(powerSet([1, 2, 3]));
